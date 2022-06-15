047a42d0aadef7fa74529855e236290662e179b4
Mariana-Palacios <marianitaaax29@gmail.com>
2022-06-15 03:34:27 +0000

    third

--- Embebidos.py
+++ Embebidos.py
@@ -1,14 +1,19 @@
-from flask import Flask
-
+from flask import Flask, request
+from flask_restful import Api
+from db_actions import get_all_data, save_data
 
 app = Flask(__name__)
+api = Api(app)
 
+@app.route('/data')
+def get_data():
+    response = get_all_data()
+    return({'data': response})
 
-@app.route('/')
-def index():
-    return 'Hola mundo'
-
+@app.route('/save', methods=['POST'])
+def put_data():
+    resp = save_data(request.json)
+    return({'data': 'ok'})
 
 if __name__ == '__main__':
-    app.run()
-
+    app.run(debug=True)
